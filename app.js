4e1d70181eb1ea5b53188bf0021d1fb4b1c529ef
thara-messeroux <tharamesseroux@gmail.com>
2025-12-03 23:47:44 -0500

    setup + Exercise 1: Find the Pokémon with number 59

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log("Hello Thara!"); 
+
+const pokemon = require('./data.js');
+
+const game = {
+    party: [],
+    gyms: [
+        { location: "Pewter City", completed: false, difficulty: 1 },
+        { location: "Cerulean City", completed: false, difficulty: 2 },
+        { location: "Vermilion City", completed: false, difficulty: 3 },
+        { location: "Celadon City", completed: false, difficulty: 4 },
+        { location: "Fuchsia City", completed: false, difficulty: 5 },
+        { location: "Saffron City", completed: false, difficulty: 6 },
+        { location: "Cinnabar Island", completed: false, difficulty: 7 },
+        { location: "Viridian City", completed: false, difficulty: 8 },
+    ],
+    items: [
+        { name: "potion", quantity: 4 },
+        { name: "pokeball", quantity: 8 },
+        { name: "rare candy", quantity: 99 },
+    ],
+}
+
+console.dir(pokemon, { maxArrayLength: null })
+
+// Exercise 1: Find the Pokémon with number 59
+const pokemon59 = pokemon.find(p => p.number === 59);
+console.log("Pokemon #59 is:", pokemon59.name);
