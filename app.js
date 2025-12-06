db0670447596834958cb5b8e7fea5bf54969714f
thara-messeroux <tharamesseroux@gmail.com>
2025-12-06 12:23:45 -0500

    exercise 7 - first attempt in progress

--- app.js
+++ app.js
@@ -51,13 +51,13 @@ Exercise 4
 Solve Exercise 4 here:
 */
 
-const starterPokemon = pokemon.find(p => p.starter === true);
+const starterPokemonBulbasaur = pokemon.find(p => p.starter === true);
 
-console.log(starterPokemon);
+console.log(starterPokemonBulbasaur);
 
-game.party.push(starterPokemon);
+game.party.push(starterPokemonBulbasaur);
 
-console.log(game);
+console.log('Exercise 4 results:', game);
 
 /*
 Exercise 5
@@ -66,14 +66,14 @@ Exercise 5
 
 Solve Exercise 5 here:
 */
-const pokemonPick1 = pokemon.find(p => p.name == 'Bulbasaur');
-const pokemonPick2 = pokemon.find(p => p.name == 'Ivysaur');
-const pokemonPick3 = pokemon.find(p => p.name == 'Venusaur');
 
-game.party.push(pokemonPick1, pokemonPick2, pokemonPick3);
+const ivysaur = pokemon.find(p => p.name == 'Ivysaur');
+const venusaur = pokemon.find(p => p.name == 'Venusaur');
+const charmander = pokemon.find(p => p.name == 'Charmander');
 
-console.log(game);
+game.party.push(ivysaur, venusaur, charmander);
 
+console.log('Exercise 5 results:', game);
 
 /*
 Exercise 6
@@ -93,13 +93,78 @@ for (const gym of game.gyms) {
 console.log("Exercise 6 results:", game.gyms);
 
 
-// Exercise 6 results: [
-//     { location: 'Pewter City', completed: true, difficulty: 1 },
-//     { location: 'Cerulean City', completed: true, difficulty: 2 },
-//     { location: 'Vermilion City', completed: false, difficulty: 3 },
-//     { location: 'Celadon City', completed: false, difficulty: 4 },
-//     { location: 'Fuchsia City', completed: false, difficulty: 5 },
-//     { location: 'Saffron City', completed: false, difficulty: 6 },
-//     { location: 'Cinnabar Island', completed: false, difficulty: 7 },
-//     { location: 'Viridian City', completed: false, difficulty: 8 }
-// ]
+/*
+Exercise 7
+1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+2. How would you replace the current starter Pokémon in your party with its evolved form?
+
+Hint: 
+  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. When working with an array of objects, the splice() array method is ideal for replacing one element with another. 
+
+
+Solve Exercise 7 here:
+*/
+
+// Find its evolved version in the big Pokémon list
+const evolvedBulbasaurtoIvysaur = pokemon.find(p => p.number === 2);
+const evolvedIvysaurToVenusaur = pokemon.find(p => p.number === 3);
+const evolvedVenusaurToCharmander = pokemon.find(p => p.number === 4);
+const evolvedCharmanderToCharmeleon = pokemon.find(p => p.number === 5);
+
+console.log(evolvedBulbasaurtoIvysaur)
+console.log(evolvedIvysaurToVenusaur)
+console.log(evolvedVenusaurToCharmander)
+console.log(evolvedCharmanderToCharmeleon)
+
+// Find the index
+const indexOfstarterPokemonBulbasaur = game.party.indexOf(starterPokemonBulbasaur);
+const indexOfevolvedBulbasaurtoIvysaur = game.party.indexOf(evolvedBulbasaurtoIvysaur);
+const indexOfevolvedIvysaurToVenusaur = game.party.indexOf(evolvedIvysaurToVenusaur);
+const indexOfevolvedVenusaurToCharmander = game.party.indexOf(evolvedVenusaurToCharmander);
+
+console.log('index:', indexOfstarterPokemonBulbasaur)
+console.log('index:', indexOfevolvedBulbasaurtoIvysaur)
+console.log('index:', indexOfevolvedIvysaurToVenusaur)
+console.log('index:', indexOfevolvedVenusaurToCharmander)
+
+// Replace the old Pokémon with the new one
+game.party.splice(indexOfstarterPokemonBulbasaur, 1, evolvedBulbasaurtoIvysaur);
+game.party.splice(indexOfevolvedBulbasaurtoIvysaur, 1, evolvedIvysaurToVenusaur);
+game.party.splice(indexOfevolvedIvysaurToVenusaur, 1, evolvedVenusaurToCharmander);
+game.party.splice(indexOfevolvedVenusaurToCharmander, 1, evolvedCharmanderToCharmeleon);
+
+console.log('Exercise 7 results, new party:', game.party);
+
+/*
+
+Exercise 7 results, new party: [
+    { number: 2, name: 'Ivysaur', type: 'grass', hp: 60, starter: false },
+    {
+        number: 3,
+        name: 'Venusaur',
+        type: 'grass',
+        hp: 80,
+        starter: false
+    },
+    {
+        number: 4,
+        name: 'Charmander',
+        type: 'fire',
+        hp: 39,
+        starter: true
+    },
+    {
+        number: 5,
+        name: 'Charmeleon',
+        type: 'fire',
+        hp: 58,
+        starter: false
+    }
+]
+
+*/
