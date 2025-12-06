8a71658728db45ddef8e2024c226ae00f71dc855
thara-messeroux <tharamesseroux@gmail.com>
2025-12-05 21:30:40 -0500

    exercise 5 - Add three selected Pokémon to game party using .find() and push()

--- app.js
+++ app.js
@@ -42,28 +42,6 @@ game.difficulty = "easy";
 console.log('Exercise 3 results:', game);
 
 /*
-Exercise 3 results: {
-    party: [],
-        gyms: [
-            { location: 'Pewter City', completed: false, difficulty: 1 },
-            { location: 'Cerulean City', completed: false, difficulty: 2 },
-            { location: 'Vermilion City', completed: false, difficulty: 3 },
-            { location: 'Celadon City', completed: false, difficulty: 4 },
-            { location: 'Fuchsia City', completed: false, difficulty: 5 },
-            { location: 'Saffron City', completed: false, difficulty: 6 },
-            { location: 'Cinnabar Island', completed: false, difficulty: 7 },
-            { location: 'Viridian City', completed: false, difficulty: 8 }
-        ],
-            items: [
-                { name: 'potion', quantity: 4 },
-                { name: 'pokeball', quantity: 8 },
-                { name: 'rare candy', quantity: 99 }
-            ],
-                difficulty: 'easy'
-}*/
-
-
-/*
 Exercise 4
 1. Select a starter Pokémon from the `pokemon` array. Remember, a starter Pokémon's `starter` property is true.
 2. Add this Pokémon to the `game.party` array. Which array method will you use to add them?
@@ -80,3 +58,17 @@ game.party.push(starterPokemon);
 
 console.log(game);
 
+/*
+Exercise 5
+1. Choose three more Pokémon from the `pokemon` array and add them to your party.
+2. Consider different attributes like 'type' or 'HP' for your selection. Which array method will you use to add them?
+
+Solve Exercise 5 here:
+*/
+const pokemonPick1 = pokemon.find(p => p.name == 'Bulbasaur');
+const pokemonPick2 = pokemon.find(p => p.name == 'Ivysaur');
+const pokemonPick3 = pokemon.find(p => p.name == 'Venusaur');
+
+game.party.push(pokemonPick1, pokemonPick2, pokemonPick3);
+
+console.log(game);
